71221ae413d4f6163d9612430930dd37a5989f39
ShorelineCrypto <admin@shorelinecrypto.com>
2020-12-27 12:20:21 -0500

    fixed timestamp attack

--- cpuminer/cheetah.py
+++ cpuminer/cheetah.py
@@ -15,9 +15,10 @@ def run_cheetah(prevheight, rpc_user,rpc_password,rpc_port, cpu):
    bcstucktime = cur_epoch - bestblock['time']
 
    if bcstucktime < 0:
-      print "Height {} block Time is ahead of current time, miner cheating or wrong timestamp {}, current {} GMT".format(bestblock['height'], bestblock['time'], datetime.utcnow())
+      print "Height {} block Time is ahead of current time {} seconds, possible miner cheating on timestamp, current {} GMT".format(bestblock['height'], bcstucktime, datetime.utcnow())
    mining_info = rpc_connection.getmininginfo()
    
+   bcstucktime = abs(bcstucktime)
    if ( bcstucktime > 120 ):
       print "ASIC/GPU miners got stuck on NENG blockchain for {} seconds".format(bcstucktime)
       print "Cheetah running, block {}: {} {} GMT".format(bestblock['height'],bestblock['time'], datetime.utcnow())
